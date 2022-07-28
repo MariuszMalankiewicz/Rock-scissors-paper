b1823b58067b8b502c5394fa375ab64bc3f896d4
Mariusz <m2ario123@wp.pl>
2022-07-28 12:15:24 +0200

    js update

--- app.js
+++ app.js
@@ -1,27 +1,55 @@
-const btns = document.querySelectorAll('.btn');
+const chooses = document.querySelectorAll('.choose-hands p');
+const play = document.querySelector('.play');
 
+let UseHand = document.getElementById('use-hand');
+let computerChoose = document.getElementById('computer-choose');
+let Rezult = document.getElementById('Rezult');
 
+let UserHand = '';
+let ComputerHand = '';
 // CHOOSE
-btns.forEach(btn => btn.addEventListener('click', (e)=>{
-    for(let i = 0; i < btns.length; i++){
-        btns[i].style.color = 'black';
+chooses.forEach(choose => choose.addEventListener('click', (e)=>{
+    for(let i = 0; i < chooses.length; i++){
+        chooses[i].style.color = 'black';
+        chooses[i].style.borderBottom = 'none';
     }
-    console.log(e.target.classList);
+    e.target.style.borderBottom = '3px solid aqua';
     e.target.style.color = "aqua";
-    const change = document.querySelector('.change');
-    let newHand = document.createElement('i');
-    newHand.classList.add(e.target.value);
-    // newHand.textContent = (e.target.textContent);
-    change.appendChild(newHand);
+    UseHand.innerHTML = e.target.textContent;
+    UserHand = e.target.textContent;
+    computerChoose.innerHTML = 'Computer';
 }));
-
-// Btn-start
-const play = document.getElementById("play");
-
-
-// function MainFunction(){
-// }
-
-// play.addEventListener('click', MainFunction);
-
-
+function ComputerChoose(){
+    ComputerHand = Math.floor(Math.random()*3);
+    if(ComputerHand === 0){
+        ComputerHand = 'Paper'
+        computerChoose.innerHTML = 'Paper';
+    }else if(ComputerHand === 1){
+        ComputerHand = 'Rock';
+        computerChoose.innerHTML = 'Rock';
+    }else if(ComputerHand === 2){
+        ComputerHand = 'Scissors';
+        computerChoose.innerHTML = 'Scissors';
+    }
+}
+function Logic(){
+    if(UserHand === 'Paper' && ComputerHand === 'Rock' || UserHand === 'Rock' && ComputerHand === 'Scissors' || UserHand === 'Scissors' && ComputerHand === 'Paper'){
+        Rezult.innerHTML = 'You Win!';
+    }else if(UserHand === 'Paper' && ComputerHand === 'Scissors' || UserHand === 'Rock' && ComputerHand === 'Paper' || UserHand === 'Scissors' && ComputerHand === 'Rock'){
+        Rezult.innerHTML = 'You Loss!';
+    }else{
+        Rezult.innerHTML = 'Draw!!';
+    }
+}
+play.addEventListener('click', ()=>{
+    if(UserHand != ''){
+        ComputerChoose();
+        Logic();
+        // console.log(`User [${UserHand}]`);
+        // console.log(`Computer [${ComputerHand}]`);
+        UserHand = '';
+        ComputerHand = '';
+    }else{
+        alert('Choose Hand');
+    }
+})
